dda047155450a18440fb1458276327399c1a041f
enginepdf <62423408+enginepdf@users.noreply.github.com>
2021-03-04 22:59:37 +0900

    UPDATE eslint_jest basic
    .eslintrc.js added

---
+++ eslint_jest basic/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true
+    },
+    "parserOptions": {
+        "ecmaVersion": 6,
+        "sourceType": "module"
+    },
+    "rules": {
+        "curly": "error",
+        "quotes": ["error", "single"],
+  	    "comma-style": ["error", "last"],
+        "object-curly-spacing": [ 0, "always" ],  // 0 === 'off' 1 === 'warn' 2 === 'error'
+        "max-len": [2, {
+                        "code" : 100
+                        }
+                   ],    
+        "semi": "error",            
+    }
+};
