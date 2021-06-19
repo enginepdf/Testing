a73666c4da5d1ccd8367d00a5b7ab8e4f0e41003
enginepdf <yeindap@gmail.com>
2021-06-20 00:00:09 +0900

    Update eslint_jest.test.js
    'done' function for asynchronous cases

--- eslint_jest basic/eslint_jest.test.js
+++ eslint_jest basic/eslint_jest.test.js
@@ -1,7 +1,8 @@
 const { appleproduct } = require("./eslint_jest");
 
-test('what product you have',() => {
+test('what product you have',(done) => {
     expect(appleproduct('notebook', 'iphone')).toBe('notebook iphone');
+    done(); // for asynchronous function done before finishing the test. or use 'return'
 });
 
 
